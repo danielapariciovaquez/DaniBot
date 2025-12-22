1920f011f4ab9305af3d14fdfe147a40f019ca8e
Daniel Aparicio Vázquez <35073235+danielapariciovaquez@users.noreply.github.com>
2025-12-22 16:39:50 +0100

    Update PruebaCan.py

--- PruebaCan.py
+++ PruebaCan.py
@@ -3,18 +3,18 @@ import time
 
 ser = serial.Serial("/dev/ttyUSB0", 2000000, timeout=1)
 
-# ENABLE
+print("Sending ENABLE frame")
 ser.write(bytes([
-    0xAA, 0x83,
+    0xAA, 0xC3,
     0x01, 0x00,
     0xF3, 0x01, 0xF5
 ]))
 
-time.sleep(0.1)
+time.sleep(0.5)
 
-# RUN 100 RPM
+print("Sending RUN 100 RPM frame")
 ser.write(bytes([
-    0xAA, 0x85,
+    0xAA, 0xC5,
     0x01, 0x00,
     0xF6, 0x80, 0x64, 0x0A, 0xE5
 ]))
