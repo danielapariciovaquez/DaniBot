076df904962f00022096954b670726a3a2ff9c1d
Daniel Aparicio Vázquez <35073235+danielapariciovaquez@users.noreply.github.com>
2025-12-26 11:22:35 +0100

    Update canProgram.py

--- canProgram.py
+++ canProgram.py
@@ -6,7 +6,7 @@ import pygame
 # CONFIGURACIÓN GENERAL
 # =====================================================
 CAN_PORT = "/dev/ttyUSB0"
-CAN_BAUD = 500000
+CAN_BAUD = 200000
 
 # IDs CAN de los motores
 MOTOR_LEFT  = [0x03, 0x04]
