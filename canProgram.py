dfe3f9981dea471233b46e922a1fdee5170ca19b
Daniel Aparicio Vázquez <35073235+danielapariciovaquez@users.noreply.github.com>
2025-12-27 14:15:39 +0100

    Update canProgram.py

--- canProgram.py
+++ canProgram.py
@@ -24,7 +24,7 @@ ALL_MOTORS  = MOTOR_LEFT + MOTOR_RIGHT
 # =====================================================
 # CONTROL
 # =====================================================
-MAX_RPM = 500
+MAX_RPM = 600
 ACC = 253
 DEADZONE = 0.001
 SEND_PERIOD = 0.05
