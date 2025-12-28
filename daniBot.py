d28efd569279a6ed38347bbbc6e5cc04aa344686
Daniel Aparicio Vázquez <35073235+danielapariciovaquez@users.noreply.github.com>
2025-12-28 14:15:04 +0100

    Actualizar daniBot.py

--- daniBot.py
+++ daniBot.py
@@ -24,7 +24,7 @@ ALL_MOTORS  = MOTOR_LEFT + MOTOR_RIGHT
 # =====================================================
 # CONTROL
 # =====================================================
-MAX_RPM = 100
+MAX_RPM = 120
 ACC = 250
 DEADZONE = 0.001
 SEND_PERIOD = 0.05
