a111e6df6f86d6fc35c45883ed31a988bf854fe8
Daniel Aparicio Vázquez <35073235+danielapariciovaquez@users.noreply.github.com>
2025-12-29 17:21:09 +0100

    Update canProgram.py

--- canProgram.py
+++ canProgram.py
@@ -214,7 +214,7 @@ try:
 
         # -------- LECTURA EJES --------
         v_cmd = apply_deadzone(-joy.get_axis(1), DEADZONE)
-        w     = apply_deadzone( joy.get_axis(3)/4, DEADZONE)
+        w     = apply_deadzone( joy.get_axis(3)/rpm_limit, DEADZONE)
 
         # -------- CONSIGNA LINEAL --------
         v_rpm_cmd = v_cmd * rpm_limit
